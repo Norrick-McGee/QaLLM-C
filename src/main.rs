f9b5433aab3938220eab0b2f10f51c8d59b20556
Norrick McGee <norrick.mcgee@gmail.com>
2023-07-17 20:09:34 -0500

    Central message logic added

--- src/main.rs
+++ src/main.rs
@@ -23,7 +23,28 @@ impl Conversation{
 fn main() {
     let data: Llama;
     // data.into_inner()
-
+    
     let conversation = Conversation::new();
+    
+    let mut context_dialog = format!(
+        "### Robot: It's me robo-buddy, how can I help you today?
+        ### Human: How big is the sun?
+        ### Robot: Sorry, I'm a robot, I can't see the sun.
+        "
+        );
+
+    // To-do: Add Dummy messages to parse and add to dialog
+
+    for message in conversation.messages.into_iter() {
+        let msg = message.text;
+        let curr_line = if message.user {
+            format!("Human: {msg}\n")
+        } else {
+            format!("Robot: {msg}\n")
+        };
+        context_dialog.push_str(&curr_line);
+    }
+    
+    // To-do: "query" the LLM bellow
 }
 
