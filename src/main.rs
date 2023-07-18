853b0a375fdb872ce5804cea8fbfab58ba060403
Norrick McGee <norrick.mcgee@gmail.com>
2023-07-17 19:27:09 -0500

    Base Conversation object added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use llm::models::Llama;
+use llm::KnownModel;
+
+
+
+struct Message{
+    user: bool,
+    text: String
 }
+
+struct Conversation{
+    messages: Vec<Message>
+}
+
+impl Conversation{
+    fn new() -> Conversation {
+        Conversation {
+            messages: Vec::new()
+        }
+    }
+}
+
+fn main() {
+    let data: Llama;
+    // data.into_inner()
+
+    let conversation = Conversation::new();
+}
+
